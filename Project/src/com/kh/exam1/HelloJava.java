a94884c5fb51682bfd88f18625a60f9db360130c
HP <HP@DESKTOP-8SLLLHB>
2021-08-02 19:48:10 +0900

    처음하는 커밋 코드

---
+++ Project/src/com/kh/exam1/HelloJava.java
@@ -0,0 +1,19 @@
+package com.kh.exam1;
+
+public class HelloJava {
+
+	public static void main(String[] args) {
+		//한 줄 주석
+		//System.out.println("Hello Java Programming!");
+		/*
+		 * 블럭 주석
+		 * System.out.println("Hello Java Programming!");
+		 * 주석 안에 있으면 실행 되지 않는다.
+		 */
+		//                 문자열(객체)를 콘솔(터미널)에 출력
+		System.out.println("Hello Java Programming!");// 출력함수(메서드라고도한다.)
+		//println은 여러줄 줄력이 되고 출력 후 개행(엔터)의 기능으로 동작
+		//System.out.print도 출력함수인데 이건 딱 한 줄만 출력해줌.
+	}
+
+}
---
+++ Project/src/com/kh/exam1/PracPrint.java
@@ -0,0 +1,24 @@
+package com.kh.exam1;
+
+public class PracPrint {
+
+	public static void main(String[] args) {
+		/*
+		 * 다음의 메세지가 출력 될 수 있도록 한다.
+		 * 1. "안녕하세요! 반갑습니다!"
+		 * 2. "인녕하세요!
+		 *     제 이름은 홍길동 입니다."
+		 */
+		//1.
+		//System.out.print("안녕하세요! 반갑습니다!");
+		//2.
+		System.out.println("안녕하세요!");
+		System.out.print("제 이름은 홍길동 입니다.");
+		/*
+		 * 밑에 줄은 println을 사용 해도 상관 없지만 print를 사용하면
+		 * 내용이 없는 맨 밑에 줄은 출력이 안되기 때문에 밑에 줄은 print 써 주는게 좀 더 정확하다.
+		 */
+	}
+	
+
+}
