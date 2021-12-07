8194e73dea2e8cfd6bd1545d8ec068ef8ce2076b
JAEJAE <onbandal@gmail.com>
2021-12-07 11:48:30 +0900

    오류 있지만 일단 커밋.

---
+++ otherproject/src/main/java/com/web/guestbook/controller/GuestBookController.java
@@ -0,0 +1,65 @@
+package com.web.guestbook.controller;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.web.guestbook.model.*;
+
+@WebServlet("/guest")
+public class GuestBookController extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+    
+    private GuestBookService service = null;
+    
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+    	service = new GuestBookService();
+    	request.setAttribute("datas", service.getList());
+    	    	
+        String view = "/WEB-INF/jsp/guestbook/index.jsp";
+        RequestDispatcher rd = request.getRequestDispatcher(view);
+        rd.forward(request, response);
+    }
+    
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        request.setCharacterEncoding("UTF-8");
+        
+        String id = request.getParameter("id");
+        String context = request.getParameter("context");
+        String ipaddr = request.getRemoteAddr();
+        
+        if(id == null) {
+	        GuestBookDTO dto = new GuestBookDTO(ipaddr, context);
+	        service = new GuestBookService();
+	        
+	        if(service.add(dto)) {
+	            response.sendRedirect("/guest");
+	        } else {
+	        	request.setAttribute("init", dto);
+	        	request.setAttribute("datas", service.getList());
+	            String view = "/WEB-INF/jsp/guestbook/index.jsp";
+	            RequestDispatcher rd = request.getRequestDispatcher(view);
+	            rd.forward(request, response);
+	        }
+        } else {
+        	GuestBookDTO dto = new GuestBookDTO();
+        	service = new GuestBookService();
+        	
+        	dto.setId(id);
+        	dto.setContext(context);
+        	if(service.modify(dto)) {
+        		response.sendRedirect("/guest");
+        	} else {
+        		response.sendRedirect("/guest");
+        	}
+        }
+    }
+}
---
+++ otherproject/src/main/java/com/web/guestbook/model/GuestBookDTO.java
@@ -0,0 +1,62 @@
+package com.web.guestbook.model;
+
+
+import java.sql.Date;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class GuestBookDTO {
+    private int id;
+    private String ipaddr;
+    private String context = "";
+    private Date date;
+    
+    public GuestBookDTO() {}
+    
+    public GuestBookDTO(String ipaddr, String context) {
+        this.ipaddr = ipaddr;
+        this.context = context;
+    }
+    
+    public int getId() {
+        return id;
+    }
+    
+    public void setId(int id) {
+        this.id = id;
+    }
+    
+    public void setId(String id) {
+    	this.id = Integer.parseInt(id);
+    }
+    
+    public String getIpaddr() {
+        return ipaddr;
+    }
+    
+    public void setIpaddr(String ipaddr) {
+        this.ipaddr = ipaddr;
+    }
+    
+    public String getContext() {
+        return context;
+    }
+    
+    public void setContext(String context) {
+        this.context = context;
+    }
+    
+    public Date getDate() {
+        return date;
+    }
+    
+    public void setDate(Date date) {
+        this.date = date;
+    }
+    public void setResultSet(ResultSet res) throws SQLException {
+    	this.id = res.getInt("G_ID");
+    	this.context = res.getString("G_CONTEXT");
+    	this.ipaddr = res.getString("G_IPADDR");
+    	this.date = res.getDate("G_DATE");
+    }
+}
