e29c6e250c5350d7f0e4aa13c70521f56feb466d
JAEJAE <onbandal@gmail.com>
2021-12-11 16:03:14 +0900

    Ajax 를 활용한 게시글 쓰기

---
+++ Project6/src/main/java/com/web/board/controller/BoardAddController.java
@@ -0,0 +1,46 @@
+package com.web.board.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.web.board.model.BoardDTO;
+import com.web.board.model.BoardService;
+
+@WebServlet("/board/add")
+public class BoardAddController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private String view ="/WEB-INF/jsp/board/add.jsp";
+
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) 
+			throws ServletException, IOException {
+		request.getRequestDispatcher(view).forward(request, response);
+	}
+
+
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) 
+			throws ServletException, IOException {
+		request.setCharacterEncoding("utf-8");
+		String bid = request.getParameter("bid");
+		String title = request.getParameter("title");
+		String content = request.getParameter("content");
+		
+		BoardDTO dto = new BoardDTO(bid, title, content);
+		BoardService service = new BoardService();
+		
+		boolean res = service.update(dto);
+		if(res) {
+			response.sendRedirect(request.getContextPath() + "/baord");
+		}else {
+			doGet(request,response);
+		}
+		
+
+
+	}
+}
