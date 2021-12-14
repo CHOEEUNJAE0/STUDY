3dbb1967c84b7c3cec8a2c7abd998b37e0eebe94
JAEJAE <onbandal@gmail.com>
2021-12-14 10:38:00 +0900

    AccountDAO를 바이바티스를 사용하는 것으로 전환

--- Project6/src/main/java/com/web/account/model/JoinDAO.java
+++ Project6/src/main/java/com/web/account/model/JoinDAO.java
@@ -8,69 +8,36 @@ import java.sql.*;
 import org.apache.ibatis.session.SqlSession;
 
 public class JoinDAO {
-    private OracleCloudConnect occ = null;// 바티스 이용하니깐 occ빼도 된다
+   // private OracleCloudConnect occ = null; 바티스 이용하니깐 occ빼도 된다
     private MybatisConnect mc = null;
     private SqlSession sess = null;
-    private final String DB = "ACCOUNTS";
+   // private final String DB = "ACCOUNTS";
+    private final String Mapper = "AccountMapper";
     
     public JoinDAO() {
-        this.occ = new OracleCloudConnect(true);
         this.mc = new MybatisConnect();
         this.sess = mc.getSession();
     }
     
     // 테이블에 데이터 추가
     public int createAccount(JoinDTO dto) {
-    	int res = 0;
-    	String q = "INSERT INTO " + DB + " VALUES("
-    			+ DB + "_SEQ.NEXTVAL, ?, ?, ?, SYSDATE, SYSDATE)";
-    								
-    	PreparedStatement st = occ.getPstat(q);
-    	try {
-    					//물음표 채우기
-			st.setString(1, dto.getUsername());
-			st.setString(2, dto.getPassword());
-			st.setString(3, dto.getEmail());
-			//정수는 setInt() , 날짜setDate() -> TO_DATE안해도 됨
-			
-			res = occ.insert(st);
-			st.close();
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
+    	//account.xml에 구문 적어주고 여기서는 구문 지워줌
+    	int res = this.sess.insert(Mapper + ".insertAccount", dto);
         return res;
     }
     
     // 테이블에서 데이터 검색(계정명으로)
     public JoinDTO findAccount(String username) {
-    	
-    	JoinDTO res = null;
-    	
-    	String query = "SELECT * FROM " + DB
-    			+ " WHERE USERNAME = ?";
-    	
-    	PreparedStatement st = occ.getPstat(query);
-    	try {
-			st.setString(1, username);
-			ResultSet rs = occ.select(st);
-			if(rs.next()) {
-				res = new JoinDTO();
-				res.setId(rs.getInt("ID"));
-				res.setUsername(rs.getString("USERNAME"));
-				res.setPassword(rs.getString("PASSWORD"));
-			}
-			rs.close();
-			st.close();
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-    	
+    		JoinDTO res = this.sess.selectOne(
+    				Mapper + ".selectAccountUsername", username);
+
     	return res;
     }
     
     // 테이블에서 데이터 검색(식별값으로)
     public JoinDTO findAccount(int id) {
-    	JoinDTO data = this.sess.selectOne("AccountMapper.selectAccount", id);//여기에 맵핑 정보가 들어가야한다.
+    	JoinDTO data = this.sess.selectOne(
+    			Mapper + ".selectAccount", id);//여기에 맵핑 정보가 들어가야한다.
     	return data;
     	//    	JoinDTO res = null;
 //    	
@@ -97,103 +64,42 @@ public class JoinDAO {
 //    	return res;
     }
     
+
     public int countAccount(JoinDTO dto) {
-    	int res = 0;
-    	
-    	String query = "SELECT COUNT(*) FROM " + DB
-    			+ " WHERE USERNAME = ?";
-    	PreparedStatement st = occ.getPstat(query);
-    	try {
-			st.setString(1, dto.getUsername());
-			ResultSet rs = occ.select(st);
-			if(rs.next()) {
-				res = rs.getInt(1);
-			}
-			rs.close();
-			st.close();
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-    	
+    	int res = this.sess.selectOne(	//하나만 세어주는거라서 selectOne
+    			Mapper+ ".selectAccountCount", dto.getUsername());
     	return res;
     }
     
     // 데이터 수정
     public int updateAccount(JoinDTO dto) {
-    	int res = 0;
-    	
-    	String query = "UPDATE " + DB + " SET"
-    			+ " PASSWORD = ?,"
-    			+ " EMAIL = ?"
-    			+ " WHERE ID = ?";
-    	
-    	PreparedStatement st = occ.getPstat(query);
-    	try {
-			st.setString(1, dto.getPassword());
-			st.setString(2, dto.getEmail());
-			st.setInt(3, dto.getId());
-			res = occ.update(st);
-			st.close();
-		} catch (SQLException e) {
-			e.printStackTrace();
-			
-		}
+    	int res = this.sess.update(Mapper + ".updateAccount",dto );
     	
     	return res;
     }
     
     // 로그인 일자를 현재 일자로 수정
     public int updateLoginDate(JoinDTO dto) {
-		int res = 0;
-		
-		String query = "UPDATE " + DB + " SET"
-				+ " LOGINDATE = SYSDATE"
-				+ " WHERE USERNAME = ?";
-		PreparedStatement st = occ.getPstat(query);
-		try {
-			System.out.println("DAO : " + dto.getUsername());
-			st.setString(1, dto.getUsername());
-			res = occ.update(st);
-			st.close();
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
+		int res = this.sess.update(Mapper + ".updateLoginDate", dto);
 		
 		return res;
 	}
     
     // 데이터 삭제
     public int removeAccount(JoinDTO dto) {
-    	int res = 0;
-    	String query = "DELETE FROM " + DB
-    			+ " WHERE USERNAME = ?"
-    			+ " AND ID = ?";
-    	
-    	PreparedStatement st = occ.getPstat(query);
-    	try {
-			st.setString(1, dto.getUsername());
-			st.setInt(2, dto.getId());
-			res = occ.delete(st);
-			st.close();
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-    	
+    	int res = this.sess.delete(Mapper + ".deleteAccount", dto);
     	return res;
     }
     
     public void commit() {
-    	occ.commit();
     	mc.commit();
     }
     
     public void rollback() {
-    	occ.rollback();
     	mc.rollback();
     }
     
     public void close() {
-    	occ.close();
     	mc.close();
     }
 
